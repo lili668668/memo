c7e9fb8b38e93becfafa29598cfc818b565fb6d6
Tzu-Yin Hong <lili668668@gmail.com>
2018-03-04 14:13:27 +0800

    Fix style

--- src/lib/show.js
+++ src/lib/show.js
@@ -4,18 +4,12 @@ const chalk = require('chalk')
 const constants= require('./constants.json')
 
 module.exports = (memo) => {
-  figlet(memo.title, (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    const memoContant = `${chalk[constants.chalkSet.titleColor](data)} \n ${chalk[constants.chalkSet.contentColor](memo.content)}`
-    console.log(boxen(memoContant, { 
-      padding: constants.boxenSet.padding, 
-      borderStyle: constants.boxenSet.style, 
-      borderColor: constants.boxenSet.color, 
-      align: constants.boxenSet.align,
-      float: constants.boxenSet.float,
-    }))
-  })
+  const memoContant = `${chalk[constants.chalkSet.titleColor].bold(memo.title)} \n ${chalk[constants.chalkSet.contentColor](memo.content)}`
+  console.log(boxen(memoContant, { 
+    padding: constants.boxenSet.padding, 
+    borderStyle: constants.boxenSet.style, 
+    borderColor: constants.boxenSet.color, 
+    align: constants.boxenSet.align,
+    float: constants.boxenSet.float,
+  }))
 }
