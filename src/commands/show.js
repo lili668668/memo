e6c300a821344120940a449a2f9e5f5225dc74c8
Tzu-Yin Hong <lili668668@gmail.com>
2018-03-04 09:04:22 +0800

    Add exception

--- src/commands/show.js
+++ src/commands/show.js
@@ -6,6 +6,12 @@ module.exports = (name) => {
   if (!fs.existsSync(constants.memoStoreFile)) return 
   const store = new jfs(constants.memoStoreFile)
   const memos = store.getSync('memos')
-  if (!name) memos.forEach(memo => show(memo))
-  else show(memos.find(memo => memo.name === name))
+  if (!name) {
+    memos.forEach(memo => show(memo))
+  }
+  else {
+    const memo = memos.find(memo => memo.name === name)
+    if (memo) show(memo)
+    else console.log(`No ${name} memo.`)
+  }
 }
